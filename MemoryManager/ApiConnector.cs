4c9f3b443f1201f5fcf558b86531c9f6b8544b08
argoneus <argoneuscze@gmail.com>
2017-07-28 13:13:56 +0200

    initial stuff

---
+++ MemoryManager/ApiConnector.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace MemoryManager
+{
+    public class ApiConnector
+    {
+        [Flags]
+        public enum ProcessAccessFlags : uint
+        {
+            All = 0x001F0FFF,
+            Terminate = 0x00000001,
+            CreateThread = 0x00000002,
+            VirtualMemoryOperation = 0x00000008,
+            VirtualMemoryRead = 0x00000010,
+            VirtualMemoryWrite = 0x00000020,
+            DuplicateHandle = 0x00000040,
+            CreateProcess = 0x000000080,
+            SetQuota = 0x00000100,
+            SetInformation = 0x00000200,
+            QueryInformation = 0x00000400,
+            QueryLimitedInformation = 0x00001000,
+            Synchronize = 0x00100000
+        }
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        private static extern IntPtr OpenProcess(ProcessAccessFlags processAccess, bool bInheritHandle, int processId);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        private static extern bool WriteProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress, byte[] lpBuffer, uint nSize, out UIntPtr lpNumberOfBytesWritten);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool CloseHandle(IntPtr hObject);
+    }
+}
---
+++ MemoryManager/MemoryHandler.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MemoryManager
+{
+    class MemoryHandler
+    {
+        private ApiConnector connector;
+
+        public MemoryHandler()
+        {
+            this.connector = new ApiConnector();
+        }
+
+        public void OpenProcess(string processName)
+        {
+        }
+
+        public void WriteToProcess(IntPtr pointer, byte[] data)
+        {
+        }
+    }
+}
